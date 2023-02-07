d0263c4e32c372423b3463a4b0c2bcd5481c7e6e
Sunday Robert <92962171+efyzhi@users.noreply.github.com>
2023-02-07 22:06:48 +0000

    Initial commit

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script setup> 
+import { ref, reactive } from "@vue/reactivity";
+import { useSetup } from "../composables/setup"
+const { colors, message, matchColor } = useSetup()
+  
+</script>
+
+<style></style>
